8b674c9b302e24b38e5e00895caa97826b1a7e41
Erica Little <eteney90@gmail.com>
2019-10-26 17:09:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-if % 3 == 0
+if int % 3 == 0
   puts "Fizz"
-elsif % 5 == 0
+elsif int % 5 == 0
   puts "Buzz"
-elsif % 15 == 0
+elsif int % 15 == 0
   puts "FizzBuzz"
 else
   puts "nil"
